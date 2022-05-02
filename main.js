e0047da386b833d657e9ded7cba90553d7756111
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2022-05-02 17:53:43 +0200

    Separate filename from slide order

--- main.js
+++ main.js
@@ -1,53 +1,67 @@
-const MAX_FILE_NUMBER = 999
-const filename = window.location.pathname.split('/').pop()
-const fileNumber = Number(filename.slice(0, 2))
+const slides = [
+  'title',
+  'agenda',
+  'why-care',
+  'why-care-unix',
+  'why-care-tools',
+  'why-care-history',
+  'why-care-paradigm',
+  'why-care-kbd',
+  'unix-intro',
+  'unix-oses',
+  'editor-intro',
+]
+const filename = window.location.pathname.split('/').pop().split('.')[0]
+const fileNumber = slides.findIndex(s => s === filename)
+
 const header = document.getElementById('header') || ({})
 const video = document.getElementById('video') || ({})
 
 const loadFile = n => {
-  if (n >= 0 && n <= MAX_FILE_NUMBER) {
-    const newFilename = String(n).padStart(2, '0') + filename.slice(2)
-    window.open(newFilename, '_self')
+  if (n >= 0 && n < slides.length) {
+    window.open(`${slides[n]}.html`, '_self')
   }
-}
-
-const generateHeader = () => {
-  return `${String(header.innerText)} (${fileNumber}/${MAX_FILE_NUMBER})`
 }
 
 (() => {
   document.title = filename
-  header.innerText = generateHeader()
+  header.innerText = `${String(header.innerText)} (${fileNumber}/${slides.length - 1})`
   video.muted = video.loop = true
-})()
 
-addEventListener('keydown', event => {
-  switch (event.code) {
-    case 'Space': // Fall-through
-    case 'ArrowRight':
-      loadFile(fileNumber + 1)
-      break
-    case 'ArrowLeft':
-      loadFile(fileNumber - 1)
-      break
-    case 'KeyX': // Toggle playback
-      if (video instanceof HTMLMediaElement) {
-        if (video.paused) {
-          video.play()
-        } else {
-          video.pause()
-        }
-      }
-      break
-    case 'KeyZ': // Rewind
-      if (video instanceof HTMLMediaElement) {
-        video.currentTime = 0
-      }
-      break
-    case 'KeyF': // Fullscreen
-      if (video instanceof HTMLMediaElement) {
-        video.requestFullscreen()
-      }
-      break
+  const mediaElementKeydownHandler = code => {
+    if (!(video instanceof HTMLMediaElement)) return
+
+    switch (code) {
+     case 'KeyX': // Toggle playback
+          if (video.paused) {
+            video.play()
+          } else {
+            video.pause()
+          }
+        break
+      case 'KeyZ': // Rewind
+          video.currentTime = 0
+        break
+      case 'KeyF': // Fullscreen
+          video.requestFullscreen()
+        break
+    }
   }
-})
+
+  addEventListener('keydown', event => {
+    switch (event.code) {
+      case 'Space': // Fall-through
+      case 'ArrowRight':
+        loadFile(fileNumber + 1)
+        break
+      case 'ArrowLeft':
+        loadFile(fileNumber - 1)
+        break
+      case 'KeyX': // Fall-through
+      case 'KeyZ': // Fall-through
+      case 'KeyF': // Handle media bindings
+        mediaElementKeydownHandler(event.code)
+        break
+    }
+  })
+})()
